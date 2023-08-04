2afc31aa18e108b2a8db2d5f451b75a657293dab
Paul Schneider <paul.schneider4@gmail.com>
2023-08-04 13:36:56 -0400

    Added Button componenent

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,3 +1,18 @@
 import { HomeScreen } from 'app/features/home/screen'
+import { GetServerSideProps } from 'next'
 
 export default HomeScreen
+
+type Repo = {
+    name: string
+    stargazers_count: number
+}
+
+export const getServerSideProps: GetServerSideProps<{
+    repo: Repo
+  }> = async () => {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const repo = await res.json()
+    console.log(repo)
+    return { props: { repo } }
+  }
--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -2,16 +2,29 @@ import { createParam } from 'solito'
 import { TextLink } from 'solito/link'
 import { Text } from 'app/design/typography'
 import { View } from 'app/design/view'
+import { useState } from 'react'
+import { Button } from 'app/design/buttton'
 
 const { useParam } = createParam<{ id: string }>()
 
 export function UserDetailScreen() {
+  const [counter, setCounter] = useState(0)
+
+  const onPress = () => {
+    console.log('Button Press')
+    setCounter(counter + 1)
+  }
+
   const [id] = useParam('id')
 
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="mb-4 text-center font-bold">{`User ID: ${id}`}</Text>
       <TextLink href="/">👈 Go Home</TextLink>
+      <View>
+        <Button title='Increment' onPress={onPress}></Button>
+        <Text>{`Counter: ${counter}`}</Text>
+      </View>
     </View>
   )
 }
